f40d61690a5d18c4840ba230a94f66810762e699
Saiful Omar <omar.saiful@gmail.com>
2012-06-10 22:13:08 +0100

    adaptive 0.2

--- src/main/webapp/js/Plugins/adaptive.js
+++ src/main/webapp/js/Plugins/adaptive.js
@@ -1,13 +1,10 @@
 if (!ORYX.Plugins) 
-    ORYX.Plugins = {};
+    ORYX.Plugins = new Object();
 
-if (!ORYX.Config)
-	ORYX.Config = {};
-
-ORYX.Plugins.Adaptive = Clazz.extend({
+ORYX.Plugins.Adaptive = ORYX.Plugins.AbstractPlugin.extend({
 	construct: function(facade){
 		this.facade = facade;
-		//this.facade.registerOnEvent(ORYX.CONFIG.EVENT_LOADED, this.adaptive.bind(this));
+
 		this.facade.offer({
 			'name': 'Adaptive',
 			'functionality': this.adaptive.bind(this),
@@ -15,77 +12,79 @@ ORYX.Plugins.Adaptive = Clazz.extend({
 			'icon': ORYX.PATH + "images/play.png",
 			'description': 'Get executed tasks',
 			'index': 2,
-			'minShape': 1,
-			'maxShape': 0,
-            'toggle': true
+            'toggle': true,
+			'minShape': 0,
+			'maxShape': 0
 		});
 	},
 	adaptive: function() {
-		ORYX.EDITOR._canvas.getChildren().each((function(child) {
-				this.applyLockingToChild(child);
-		}).bind(this));
+			var canvas = this.facade.getCanvas();
+		
+			var id = canvas.properties["oryx-id"];
+			//alert(id);	
+			
+			Ext.Ajax.request({
+            url: "http://localhost:8080/gwt-console-server/rs/process/definition/"+ id + "/instances",
+            method: 'GET',
+            success: function(response){
+    	   		try {   	   			
+    	   			if(response.responseText && response.responseText.length > 0) {
+	   	   				var instancesjson = response.responseText.evalJSON();
+    	   				var instancesobj = instancesjson["instances"];
+ 
+    		        	for(var i=0;i<instancesobj.length;i++){
+    	   					var toapplyobj = instancesobj[i];
+	    	   				//alert(instancesobj[i].id);
+	    	   				//alert(instancesobj[i].rootToken.currentNodeName);
+    	   					this.checkNodes(toapplyobj);
+    	   					//this.checkNodes.bind(this, instancesobj[i])
+    	   				}
+	   	   			} else {
+	    	   				Ext.Msg.alert('Invalid Instances data1.');
+	    	   			}
+	    	   		} catch(e) {
+	    	   			Ext.Msg.alert('Error applying Instances data2:\n' + e);
+	    	   		}
+	            }.bind(this),
+	            failure: function(){
+	            	Ext.Msg.alert('Error applying Instances data.');
+	            }
+         	});		
 	},
-	applyLockingToChild: function(child) {
-		if(child && (child instanceof ORYX.Core.Node || child instanceof ORYX.Core.Edge) && child.properties["oryx-isselectable"] == "false") {
-			child.setSelectable(false);
-			child.setMovable(false);
+
+	checkNodes: function(toapplyobj) {
+		var canvas = this.facade.getCanvas();
+
+		ORYX.EDITOR._canvas.getChildNodes().each((function(child) {
+				if(child.properties["oryx-name"] == toapplyobj.rootToken.currentNodeName){
+					//alert(child.properties["oryx-name"]);
+					child.setProperty("oryx-bordercolor", "#888888");
+    				child.setProperty("oryx-bgcolor", "#CCEEFF");
+    		   		child.setSelectable(false);
+		    		child.setMovable(false);
+		    		child.refresh();
+/*    				if(child.getChildren().size() > 0) {
+						for (var i = 0; i < shape.getChildren().size(); i++) {
+							if(child.getChildren()[i] instanceof ORYX.Core.Node || child.getChildren()[i] instanceof ORYX.Core.Node) {
+								this.lockShape(shape.getChildren()[i]);
+							}
+						}
+    				}*/
+	    	}
+    	}).bind(this));
+		
+		/*var childthemestr = themeObj[childgroup];
+		if(childthemestr && child.properties["oryx-isselectable"] != "false") { 
+			var themestrparts = childthemestr.split("|");
+			child.setProperty("oryx-bgcolor", themestrparts[0]);
+			child.setProperty("oryx-bordercolor", themestrparts[1]);
+			child.setProperty("oryx-fontcolor", themestrparts[2]);
 			child.refresh();
 		}
-		if(child && child.getChildren().size() > 0) {
-			for (var i = 0; i < child.getChildren().size(); i++) {
-				this.applyLockingToChild(child.getChildren()[i]);
+		if(child.getChildNodes().size() > 0) {
+			for (var i = 0; i < child.getChildNodes().size(); i++) {
+				this.applyThemeToNodes(child.getChildNodes()[i], themeObj);
 			}
-		}
-	},
-    locknodes: function() {
-    	var selection = this.facade.getSelection();
-    	selection.each(function(shape) {
-    		this.lockShape(shape);
-    	}.bind(this));
-        
-    },
-    unlocknodes: function() {
-    	var selection = this.facade.getSelection();
-    	selection.each(function(shape) {
-    		this.unlockShape(shape);
-    	}.bind(this));
-    },
-    unlockShape: function(shape) {
-    	if(shape) {
-	    	shape.setSelectable(true);
-	    	shape.setMovable(true);
-	    	if(shape instanceof ORYX.Core.Node || shape instanceof ORYX.Core.Edge) {
-	    		shape.setProperty("oryx-bordercolor", shape.properties["oryx-origbordercolor"]);
-	    		shape.setProperty("oryx-bgcolor", shape.properties["oryx-origbgcolor"]);
-	    	}
-	    	shape.setProperty("oryx-isselectable", "true");
-	    	shape.refresh();
-	    	if(shape.getChildren().size() > 0) {
-				for (var i = 0; i < shape.getChildren().size(); i++) {
-					if(shape.getChildren()[i] instanceof ORYX.Core.Node || shape.getChildren()[i] instanceof ORYX.Core.Node) {
-						this.unlockShape(shape.getChildren()[i]);
-					}
-				}
-			}
-    	}
-    },
-    lockShape: function(shape) {
-    	if(shape) {
-    		shape.setSelectable(false);
-    		shape.setMovable(false);
-    		if(shape instanceof ORYX.Core.Node || shape instanceof ORYX.Core.Edge) {
-    			shape.setProperty("oryx-bordercolor", "#888888");
-    			shape.setProperty("oryx-bgcolor", "#CCEEFF");
-    		}
-    		shape.setProperty("oryx-isselectable", "false");
-    		shape.refresh();
-    		if(shape.getChildren().size() > 0) {
-				for (var i = 0; i < shape.getChildren().size(); i++) {
-					if(shape.getChildren()[i] instanceof ORYX.Core.Node || shape.getChildren()[i] instanceof ORYX.Core.Node) {
-						this.lockShape(shape.getChildren()[i]);
-					}
-				}
-			}
-    	}
-    }
+		}*/
+	}
 });
